aeed7eac72f081de538f9786976a6639805c3097
Javier Cavalero  <javicavalero56@mail.com>
2022-08-14 11:03:55 -0300

    manejando errores de api con sweetAlert

--- src/components/Listado.js
+++ src/components/Listado.js
@@ -1,48 +1,59 @@
-import {useEffect, useState} from 'react';
-import { Link,Navigate} from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { Link, Navigate } from 'react-router-dom';
 import axios from 'axios';
+import swAlert from '@sweetalert/with-react';
 
 function Listado() {
 
-let token = localStorage.getItem('token');
+  let token = localStorage.getItem('token');
 
-const [moviesList, setMoviesList] = useState([]);
+  const [moviesList, setMoviesList] = useState([]);
 
-useEffect(() =>{
-const endPoint = 'https://api.themoviedb.org/3/discover/movie?api_key=85767dd9b89a27e1fc5162a1145bd542&language=es-ES&page=1'
-axios.get(endPoint)
-.then(res =>{
-  const apiData = res.data.results;
-setMoviesList(apiData);
-})
-},[setMoviesList] );
+  useEffect(() => {
+    const endPoint = 'https://api.themoviedb.org/3/discover/movie?api_key=85767dd9b89a27e1fc5162a1145bd542&language=es-ES&page=1'
+    axios.get(endPoint)
+      .then(res => {
+        const apiData = res.data.results;
+        setMoviesList(apiData);
+      })
+      .catch(err => {
+        swAlert(<h2>Hubo un error al intentar cargar la página, vuelva a intentarlo nuevamente más tarde...</h2>);
+      })
+  }, [setMoviesList]);
 
-console.log(moviesList);
 
-if (!token) {
-   return <Navigate to={'/'}  />
-}
+  if (!token) {
+    return <Navigate to={'/'} />
+  }
 
 
 
   return (
-    <>
+
     <div className='row'>
-        <div className='col-3'>
+      {moviesList.map((oneMovie, idx) => {
+        return (
+          <div className='col-3 my-3' key={idx}>
             <div className="card" >
-  <img src="..." className="card-img-top" alt="..."/>
-  <div className="card-body">
-    <h5 className="card-title">Movie title</h5>
-    <p className="card-text">Review movie</p>
-    <Link to="/" className="btn btn-primary">Ver detalle</Link>
-  </div>
-</div>
- </div>
-       
+              <img src={`https://image.tmdb.org/t/p/w500/${oneMovie.poster_path}`} className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h5 className="card-title">{oneMovie.title.substring(0,20)}...</h5>
+                <p className="card-text">{oneMovie.overview.substring(0,50)}...</p>
+                <Link to="/" className="btn btn-primary">Ver detalle</Link>
+              </div>
+            </div>
+          </div>
+        )
+
+      })
+
+      }
 
     </div>
-    </>
+
   )
 }
 
-export default Listado
+export default Listado;
+
+
